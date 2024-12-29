86dacf9055dbefae077ac286ca481750b433d185
Edvard Kirakosyan <kirakosyan3010@gmail.com>
2024-12-29 23:07:11 +0400

    c

--- main.go
+++ main.go
@@ -3,18 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	conferenceName := "GO conference"
-	const conferenceTickets = 50
-	remaininTickets := 50
+	var conferenceName string = "GO conference"
+	const conferenceTickets int = 50
+	remainingTickets := 50
+
+	fmt.Printf("conf tickets is %T, remaining tickets is %T, conf name Is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
 	fmt.Printf("Welcome to our %v booking app\n", conferenceName)
-	fmt.Printf("We have %v tickets and %v are still available\n", conferenceTickets, remaininTickets)
+	fmt.Printf("We have %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets")
 
-	var userName string
-
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
 	// ask for name
-	userName = "Tom"
-	fmt.Println(userName)
+	fmt.Println("Enter your firstName:")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your lastName:")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email:")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets:")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("thank you %v %v for booking %v tickets.\nYou will recieve them on %v\n", firstName, lastName, userTickets, email)
 
 }
