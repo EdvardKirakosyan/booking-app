ad2fc6d09a58e4ba909dab9ff0105a30dd96b1c8
Edvard Kirakosyan <kirakosyan3010@gmail.com>
2024-12-29 22:38:54 +0400

    c

--- main.go
+++ main.go
@@ -11,4 +11,10 @@ func main() {
 	fmt.Printf("We have %v tickets and %v are still available\n", conferenceTickets, remaininTickets)
 	fmt.Println("Get your tickets")
 
+	var userName string
+
+	// ask for name
+	userName = "Tom"
+	fmt.Println(userName)
+
 }
