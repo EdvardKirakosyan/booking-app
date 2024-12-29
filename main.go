00ea9888275c984961ff524c6af72cb2276bc993
Edvard Kirakosyan <kirakosyan3010@gmail.com>
2024-12-29 22:26:53 +0400

    initial

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	conferenceName := "GO conference"
+
+	fmt.Println("Welcome to our", conferenceName, "booking app")
+	fmt.Println("Get your tickets")
+
+}
