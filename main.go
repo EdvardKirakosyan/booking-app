b36027b4395913c428d93c69695f73f885cad2ce
Edvard Kirakosyan <kirakosyan3010@gmail.com>
2024-12-29 22:36:10 +0400

    c

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import "fmt"
 
 func main() {
 	conferenceName := "GO conference"
+	const conferenceTickets = 50
+	remaininTickets := 50
 
-	fmt.Println("Welcome to our", conferenceName, "booking app")
+	fmt.Printf("Welcome to our %v booking app\n", conferenceName)
+	fmt.Printf("We have %v tickets and %v are still available\n", conferenceTickets, remaininTickets)
 	fmt.Println("Get your tickets")
 
 }
